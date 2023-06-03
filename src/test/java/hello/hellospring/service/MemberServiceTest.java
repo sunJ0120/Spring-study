c6add66b9da327a60cdf20ca9cfb105a37dba31f
BOOK-S08D1UN6CT\sspure1214 <sspure1214@naver.com>
2023-06-03 20:31:26 +0900

    spring 공부_회원 서비스 개발
    회원 서비스 테스트
    
    회원가입, 중복_회원_예외, 등으로 이뤄져 있으며 현재 AfterEach 구현중
    
    AfterEach로 테스트 케이스의 데이터를 계속 비워주기 위해 MemoryMemberRepository 객체를 생성해 객체 속에 clear 사용. BeforeEach를 이용해 실행되기 전에 MemberService와 memberRepository를 생성해서 생성자로 넣는 작업을 반복한다.
    
    memberService 객체에서 repository를 new로 생성하는 것이 아닌, 생성자에 넣는 방식으로 구성하여 new로 생성했을 때 다른 객체에서 실행되는 문제점을 수정

--- src/test/java/hello/hellospring/service/MemberServiceTest.java
+++ src/test/java/hello/hellospring/service/MemberServiceTest.java
@@ -1,16 +1,25 @@
 package hello.hellospring.service;
 import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class MemberServiceTest {
-    MemberService memberService = new MemberService();
-    MemoryMemberRepository memberRepository = new MemoryMemberRepository();
 
+    MemberService memberService;
+    MemberRepository memberRepository;
+
+    @BeforeEach
+    public void beforeEach(){
+        memberRepository = new MemoryMemberRepository();
+        memberService = new MemberService(memberRepository);
+    }
     @AfterEach
     public void afterEach(){
         memberRepository.clearStore();
@@ -45,6 +54,7 @@ class MemberServiceTest {
         memberService.join(member1);
         IllegalStateException e = assertThrows(IllegalStateException.class, () -> memberService.join(member2));
         assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
+
         /*
             try {
                 memberService.join(member2);
