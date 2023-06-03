cad0bf067a3f6764fa177ce741720fe67cbef13a
BOOK-S08D1UN6CT\sspure1214 <sspure1214@naver.com>
2023-06-03 20:20:07 +0900

    spring 공부_회원 서비스 개발
    회원 서비스 테스트
    
    회원가입, 중복_회원_예외, 등으로 이뤄져 있으며 현재 AfterEach 구현중
    
    AfterEach로 테스트 케이스의 데이터를 계속 비워주기 위해 MemoryMemberRepository 객체를 생성해허 객체 속에 clear 사용.
    
    주로 사용하는 memberService 객체와 다른 객체를 생성해서 clear하는 것이라는 문제 존재.

--- src/test/java/hello/hellospring/service/MemberServiceTest.java
+++ src/test/java/hello/hellospring/service/MemberServiceTest.java
@@ -1,13 +1,20 @@
 package hello.hellospring.service;
 import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemoryMemberRepository;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class MemberServiceTest {
-
     MemberService memberService = new MemberService();
+    MemoryMemberRepository memberRepository = new MemoryMemberRepository();
+
+    @AfterEach
+    public void afterEach(){
+        memberRepository.clearStore();
+    }
 
     @Test
     void 회원가입() {
