84cc0609871e11f888e78eb7ead2737a7d7f70f6
BOOK-S08D1UN6CT\sspure1214 <sspure1214@naver.com>
2023-06-04 17:10:18 +0900

    spring 공부_회원 서비스 개발
    컴포넌트 자동 의존관계 주입_repository
    생성시 컨테이너에 담기 위해 @Repository 어노테이션을 사용

--- src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
+++ src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -1,11 +1,14 @@
 package hello.hellospring.repository;
 import hello.hellospring.domain.Member;
+import org.springframework.stereotype.Repository;
+
 import java.util.*;
 
 
 
 
 
+@Repository
 public class MemoryMemberRepository implements MemberRepository{
 
     /*
