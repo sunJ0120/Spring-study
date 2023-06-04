67ed4604c99cd104ac0d4cded4383e2ad9b8a108
BOOK-S08D1UN6CT\sspure1214 <sspure1214@naver.com>
2023-06-04 19:01:55 +0900

    spring 공부_회원 서비스 개발
    자바 코드로 의존관계 직접 등록_필드 DI
    DI 할때 필드에 직접 @Autowired를 붙여서 표현

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -6,11 +6,11 @@ import org.springframework.stereotype.Controller;
 
 @Controller
 public class MemberController {
-    private final MemberService memberService;
+    @Autowired private MemberService memberService;
 
-    @Autowired
-    public MemberController(MemberService memberService){
-        this.memberService = memberService;
-    }
+//    @Autowired
+//    public MemberController(MemberService memberService){
+//        this.memberService = memberService;
+//    }
 
 }
