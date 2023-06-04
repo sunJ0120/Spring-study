b9c1dc269a7442abdd195e8bd6b260a3c3944aa9
BOOK-S08D1UN6CT\sspure1214 <sspure1214@naver.com>
2023-06-04 19:04:51 +0900

    spring 공부_회원 서비스 개발
    자바 코드로 의존관계 직접 등록_setter DI
    DI 할때 setXX를 직접 생성해서 DI를 하는 방식

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -6,7 +6,11 @@ import org.springframework.stereotype.Controller;
 
 @Controller
 public class MemberController {
-    @Autowired private MemberService memberService;
+    private MemberService memberService;
+
+    public void setMemberService(MemberService memberService){
+        this.memberService = memberService;
+    }
 
 //    @Autowired
 //    public MemberController(MemberService memberService){
