a4445b6c3be839d7aa34b557a43b04d2626d60f8
BOOK-S08D1UN6CT\sspure1214 <sspure1214@naver.com>
2023-06-06 21:40:43 +0900

    spring 공부_JPA data
    JPA data에서 선언한 인터페이스 레포지토리를 적용하기 위해 변경
    memberRepository를 선언하여 memberService에 인자로 넘겨주었다.

--- src/main/java/hello/hellospring/service/SpringConfig.java
+++ src/main/java/hello/hellospring/service/SpringConfig.java
@@ -1,33 +1,29 @@
 package hello.hellospring.service;
 import hello.hellospring.repository.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.transaction.annotation.Transactional;
-
-import javax.persistence.EntityManager;
 
 @Configuration
 public class SpringConfig {
 
-//    private final DataSource dataSource;
-//    public SpringConfig(DataSource dataSource) {
-//        this.dataSource = dataSource;
-//    }
+    private final MemberRepository memberRepository;
 
-    private final EntityManager em;
-    public SpringConfig(EntityManager em) {
-        this.em = em;
+    @Autowired
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
+
     @Bean
     public MemberService memberService(){
-        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
     }
-    @Bean
-    public MemberRepository memberRepository(){
+//    @Bean
+//    public MemberRepository memberRepository(){
 //        return new MemoryMemberRepository();
 //        return new JdbcMemberRepository(dataSource);
 //        return new JdbcTemplateMemberRepository(dataSource);
-        return new JpaMemberRepository(em);
-    }
+//        return new JpaMemberRepository(em);
+//    }
 }
 
