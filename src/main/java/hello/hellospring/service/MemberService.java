e0be2d25f84d143f0fe57f89c95ab447c3117518
BOOK-S08D1UN6CT\sspure1214 <sspure1214@naver.com>
2023-06-07 15:38:13 +0900

    spring 공부_AOP가 필요한 상황
    AOP가 필요한 상황을 연출하기 위해 시간을 구하는 method를 추가
    현재 spring test 실행하면 오류가 발생하지만, spring을 제대로 배운 것이 아니기 때문에 @Bean 관련 의존성을 해결할 수 없음. 제대로 배운 후에 다시 오기

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -24,11 +24,20 @@ public class MemberService {
      */
 
     public Long join(Member member){
-        //같은 이름이 있는 중복 회원x
-        validateDuplicateMember(member); //중복회원 검증
 
-        memberRepository.save(member);
-        return member.getId();
+        long start = System.currentTimeMillis();
+
+        try{
+            //같은 이름이 있는 중복 회원x
+            validateDuplicateMember(member); //중복회원 검증
+            memberRepository.save(member);
+            return member.getId();
+        } finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("join " + timeMs + "ms");
+        }
+
     }
 
     private void validateDuplicateMember(Member member) {
@@ -43,9 +52,17 @@ public class MemberService {
      */
 
     public List<Member> findMembers(){
-        return memberRepository.findAll();
-    }
 
+        long start = System.currentTimeMillis();
+
+        try{
+            return memberRepository.findAll();
+        }finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("findMembers " + timeMs + "ms");
+        }
+    }
     public Optional<Member> findOne(Long memberId){
         return memberRepository.findById(memberId);
     }
